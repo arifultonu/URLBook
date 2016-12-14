82dfa023516c230c556534e3f64ccc6ba67e4829
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2016-12-14 11:06:02 -0800

    Table View Successfully run

---
+++ URLBook/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  URLBook
+//
+//  Created by Ariful Tonu on 12/13/16.
+//  Copyright © 2016 AndApps. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item){
+        title.text = item.title
+        category.text = item.category
+        details.text = item.details
+    }
+    
+    
+}
