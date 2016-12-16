5535d41a1ab88b1ed96489fc321e31ea1383fca1
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2016-12-16 10:07:57 -0800

    Add Detail view

---
+++ URLBook/ItemDatailsVC.swift
@@ -0,0 +1,35 @@
+//
+//  ItemDatailsVC.swift
+//  URLBook
+//
+//  Created by Ariful Tonu on 12/16/16.
+//  Copyright © 2016 AndApps. All rights reserved.
+//
+
+import UIKit
+
+class ItemDatailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
