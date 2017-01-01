c3c7096960b23b940d95c7c4278b4a1cbcef1b58
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2017-01-01 09:13:14 -0800

    web view completed it self

--- URLBook/ItemDetailWebVC.swift
+++ URLBook/ItemDetailWebVC.swift
@@ -19,27 +19,41 @@ class ItemDetailWebVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let topNavigationItem = self.navigationController?.navigationBar.topItem{
-            topNavigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
-        }
-    
+//        if let topNavigationItem = self.navigationController?.navigationBar.topItem {
+//            topNavigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+//        }
+        
+       SearchBar.text = "http://"
+        
     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func searchBarSearchButtonClicked(_ searchbar: UISearchBar){
+    
+        searchbar.resignFirstResponder()
+        
+        let text = SearchBar.text
+        let webUrl = URL(string: text!)
+        let webUrlRequest = URLRequest(url: webUrl!)
+        WebView.loadRequest(webUrlRequest)
     }
-    */
 
+    func webViewDidStartLoad(_ : UIWebView) {
+        
+        ActInd.startAnimating()
+        
+    }
+    
+    func webViewDidFinishLoad(_ : UIWebView) {
+        
+        ActInd.stopAnimating()
+        
+    }
 }
